5f4c70fce4734cc86f4ee0922c8ef87529ef1814
Eric Vallee <eric_vallee@webificservices.com>
2021-03-08 15:15:04 -0500

    fix: #264 More accurately renamed 'arranger' url environment variable to api url

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ export KEYCLOAK_CLIENT_ID=${KEYCLOAK_CLIENT_ID:-cqdg-client}
 
 cat > /usr/share/nginx/html/config.js <<- EndOfConf
 window.env = {
-    REACT_APP_API_URL:"$PORTAL_URL",
+    REACT_APP_API_URL:"$API_URL",
     REACT_APP_KEYCLOAK_CONFIG:{"realm": "$KEYCLOAK_REALM", "url": "$KEYCLOAK_URL", "clientId": "$KEYCLOAK_CLIENT_ID"}
 }
 EndOfConf
