74a4261b0856beebeecdb80e0332707976d595dc
Eric Vallee <eric_vallee@webificservices.com>
2021-03-08 15:55:19 -0500

    fix: #264 Expected format fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ export KEYCLOAK_CLIENT_ID=${KEYCLOAK_CLIENT_ID:-cqdg-client}
 
 cat > /usr/share/nginx/html/config.js <<- EndOfConf
 window.env = {
-    REACT_APP_API_URL:"$API_URL",
-    REACT_APP_KEYCLOAK_CONFIG:{"realm": "$KEYCLOAK_REALM", "url": "$KEYCLOAK_URL", "clientId": "$KEYCLOAK_CLIENT_ID"}
+    REACT_APP_API_URL: "$API_URL",
+    REACT_APP_KEYCLOAK_CONFIG: JSON.stringify({"realm": "$KEYCLOAK_REALM", "url": "$KEYCLOAK_URL", "clientId": "$KEYCLOAK_CLIENT_ID"})
 }
 EndOfConf
 
