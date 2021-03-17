5dfb22b48882b20c03bf2d319bbee7c68650d4d0
Kevin Jacques <kjacques@ferlab.bio>
2021-03-17 15:49:04 -0400

    fix: #290 facette sidebar

--- src/components/layouts/Sidebar/index.tsx
+++ src/components/layouts/Sidebar/index.tsx
@@ -13,7 +13,7 @@ interface ISidebar {
 }
 
 const SideBar: React.FC<ISidebar> = ({ children, className = '' }) => {
-    const [isCollapsed, setIsCollapsed] = useState(false);
+    const [isCollapsed, setIsCollapsed] = useState(true);
     const intl = useIntl();
 
     const isCollapsedClassName = classNames({ 'side-panel-collapsed': isCollapsed });
@@ -21,7 +21,7 @@ const SideBar: React.FC<ISidebar> = ({ children, className = '' }) => {
         <div className={`side-panel ${isCollapsedClassName} ${className}`}>
             <div className="side-panel-header">
                 <Button onClick={() => setIsCollapsed(!isCollapsed)} type="text">
-                    {isCollapsed ? <AiOutlineMenu /> : <AiOutlineMenuFold />}
+                    {isCollapsed ? <AiOutlineMenu size={24} /> : <AiOutlineMenuFold size={24} />}
                 </Button>
             </div>
             <div className="side-panel-content">{children}</div>
